b9afd4870019b68878c2d7b7848833c5d6c86466
Ruby0760 <rubygautam0760@gmail.com>
2022-08-18 12:01:28 +0530

    first commit

---
+++ smartcity.js
@@ -0,0 +1,17 @@
+<script src="https://www.gstatic.com/firebasejs/8.4.3/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.4.3/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.4.3/firebase-database.js"></script>
+
+<script>
+  var firebaseConfig = {
+    apiKey: "AIzaSyAiQ5Zam18dCLue80FpdtMUEJR9_J-pcdw",
+    authDomain: "sign-in-79835.firebaseapp.com",
+    projectId: "sign-in-79835",
+    storageBucket: "sign-in-79835.appspot.com",
+    messagingSenderId: "556975289669",
+    appId: "1:556975289669:web:0313e9533b48edd56bba2c",
+    measurementId: "G-54BVD4VC16"
+  };
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
